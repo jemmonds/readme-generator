88f0ad915431e3b2b3c27fa46e0b04407a4c10e5
Your Name <you@example.com>
2024-02-19 19:59:35 +0000

    Added markdown

--- index.js
+++ index.js
@@ -2,54 +2,63 @@ const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
-
 // array of questions for user
-const questions = [    {
-  type: "input",
-  name: "title",
-  message: "What do you want the title of your project to be? "
-},
-{
-type: "input",
-name: "description",
-message: "Write a description for your project? "
-},
-{
-  type: "input",
-  name: "installation",
-  message: "What needs to be installed to be able to run this program?",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "How should this be used?",
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What do you want the title of your project to be?"
     },
     {
-      type: "list",
-      name: "License",
-      message: "Which license would you like to use?",
-      choices: ["MIT License","GNU","BSD-2-Clause","Boost Software"],
+        type: "input",
+        name: "description",
+        message: "What is the description of your project?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What needs to be installed to run this programme?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How do you plan for this programme to be used?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Which license would you like to use?",
+        choices: ["MIT License", "GNU", "BSD-2-Clause", "Boost Software"],
+    },
+    {
+        type: "input",
+        name: "git",
+        message: "What is your Git username?"
+    },
+    {
+        type: "input",
+        name: "contributions",
+        message: "Are you open to contributions, Yes or No? If yes, please state what contributions you would like to be made."
+    },
+    {
+      type: "input",
+      name: "questions",
+      message: "What is your email address? This will be used for people to contact you to ask questions about your project"
   },
-
-  {
-    type: "input",
-    name: "contributions",
-    message: "Are you open to contributions?",
-},
+    
 ];
-
-
 // function to write README file
 function writeToFile(fileName, data) {
 }
 // function to initialize program
 function init() {
-    inquirer.prompt(questions) .then((answers) => {
-      const exampleFile = `<div> ${answers.title} </div> <div> ${answers.description} </div> <div> ${answers.installation} </div> <div> ${answers.installation}</div> <div> ${answers.usage} </div> <div> ${answers.License} </div>`;
-
-      fs.writeFile("log.html", exampleFile, (err) => err ? console.log(err) : console.log("Success!"));
-  }
-  );
+    inquirer.prompt(questions).then(
+        (answers) => {
+            console.log(answers);
+            const exampleFile = generateMarkdown(answers);
+            fs.writeFile("README2.md", exampleFile, (err) => err ? console.log(err) : console.log("Success!"));
+        }
+    );
 }
 // function call to initialize program
 init();
